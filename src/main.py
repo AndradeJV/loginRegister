da7166c3da33cfde4b704c133c19a98d88a502de
João Vitor Andrade de Araujo <joaovandrade30@gmail.com>
2021-04-19 18:42:53 -0300

    Inicio do login
    Tkinter criado e começo das operações

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+
+
+def toEnter():
+    sistemaLogin["text"] = "Logado"
+
+
+sistemaLogin = Tk()
+
+nomeEmpresa = Label(sistemaLogin, text="Mercado Eletrônico", font="Tahoma 25")
+nomeEmpresa.place(x=250, y=100)
+
+botao_logar = Button(sistemaLogin, width=25, text="Logar", command=toEnter)
+botao_logar.place(x=300, y=400)
+
+sistemaLogin.geometry("800x600+0+0")
+sistemaLogin.mainloop()
