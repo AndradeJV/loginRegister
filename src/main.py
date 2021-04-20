1d3d85a2cabec8abf110427b2b16b2b5495b35ae
João Vitor Andrade de Araujo <joaovandrade30@gmail.com>
2021-04-20 01:22:16 -0300

    Criação de funções e classe
    Necessitam ainda de verificações para reparação de gaps;

--- src/main.py
+++ src/main.py
@@ -5,23 +5,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-driver = webdriver.Chrome(
-    executable_path=r'D:\Programas\chromedriver.exe')
+class robotAutomation():
+    def CreateUser():
+        driver = webdriver.Chrome(
+            executable_path=r'D:\Programas\chromedriver.exe')
 
-driver.get(
-    "http://automationpractice.com/index.php?controller=authentication&back=my-account")
+        driver.get(
+            "http://automationpractice.com/index.php?controller=authentication&back=my-account")
+
+        driver.find_element_by_id("email_create").send_keys(
+            "joaovandrade3@gmail.com")
+
+        driver.find_element_by_id("SubmitCreate").click()
+
+        def completeCreateUser(self):
+            driver.find_element_by_id("id_gender1").click()
+
+            time.sleep(2)
+
+            driver.find_element_by_id("customer_firstname").send_keys('Joao')
+            driver.find_element_by_id("customer_lastname").send_keys('Andrade')
+            driver.find_element_by_id("passwd").send_keys('123456')
 
 
-driver.find_element_by_id("email_create").send_keys(
-    "joaovandrade3@gmail.com")
-
-driver.find_element_by_id("SubmitCreate").click()
-time.sleep(10)
-
-driver.find_element_by_id("id_gender1").click()
-
-time.sleep(2)
-
-driver.find_element_by_id("customer_firstname").send_keys('Joao')
-driver.find_element_by_id("customer_lastname").send_keys('Andrade')
-driver.find_element_by_id("passwd").send_keys('123456')
+robotAutomation.CreateUser()
+robotAutomation.completeCreateUser()
