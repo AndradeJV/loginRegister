f713054d545dd294c4583ad543b8e6652d61fe98
João Vitor Andrade de Araujo <joaovandrade30@gmail.com>
2021-04-22 14:33:29 -0300

    Verificações no DB
    Verificações criadas para cada usuário no db a partir de um email

--- src/main.py
+++ src/main.py
@@ -23,14 +23,6 @@ if conDb.is_connected():
     recebeDb = cursor.fetchone()
     print("Conectado ao db ", recebeDb)
 
-"""
-if conDb.is_connected():
-    cursor.close()
-    conDb.close()
-    print("Conexão encerrada")
-"""
-
-# import login
 
 resp = int(input('Novo cadastro (1- Sim, 2-Não)?'))
 
@@ -38,53 +30,72 @@ resp = int(input('Novo cadastro (1- Sim, 2-Não)?'))
 if resp == 1:
 
     recebeEmail = input('Digite um email:')
-    # Variável para receber o path e dar acesso ao google chrome
-    driver = webdriver.Chrome(executable_path=r'D:\Programas\chromedriver.exe')
 
-    # get para abrir o navegador e no caso, o chrome
-    driver.get(
-        "http://automationpractice.com/index.php?controller=authentication&back=my-account")
+    comandoSql = "SELECT * FROM usuario_login WHERE email = %s"
+    valores = (recebeEmail,)
+    cursor.execute(comandoSql, valores)
 
-    # seleção de elementos para manipular o DOM da página web
+    linha = cursor.fetchone()
 
-    driver.find_element_by_id("email_create").send_keys(recebeEmail)
+    if linha is None:
 
-    driver.find_element_by_id("SubmitCreate").click()
+        # Variável para receber o path e dar acesso ao google chrome
+        driver = webdriver.Chrome(
+            executable_path=r'D:\Programas\chromedriver.exe')
 
-    # Todos os "times.sleep's"  foram colocados para evitar bugs na automação dos processos "
+        # get para abrir o navegador e no caso, o chrome
+        driver.get(
+            "http://automationpractice.com/index.php?controller=authentication&back=my-account")
 
-    time.sleep(10)
+        # seleção de elementos para manipular o DOM da página web
 
-    driver.find_element_by_id("id_gender1").click()
+        driver.find_element_by_id("email_create").send_keys(recebeEmail)
 
-    time.sleep(2)
+        driver.find_element_by_id("SubmitCreate").click()
 
-    driver.find_element_by_id("customer_firstname").send_keys('Joao')
-    driver.find_element_by_id("customer_lastname").send_keys('Andrade')
-    driver.find_element_by_id("passwd").send_keys('123456')
+        # Todos os "times.sleep's"  foram colocados para evitar bugs na automação dos processos "
 
-    time.sleep(2)
+        time.sleep(10)
 
-    driver.find_element_by_id("days").send_keys('1')
-    driver.find_element_by_id("months").send_keys('setembro')
-    driver.find_element_by_id("years").send_keys('1994')
+        driver.find_element_by_id("id_gender1").click()
 
-    time.sleep(2)
+        time.sleep(2)
 
-    driver.find_element_by_id("optin").click()
-    driver.find_element_by_id("newsletter").click()
+        driver.find_element_by_id("customer_firstname").send_keys('Joao')
+        driver.find_element_by_id("customer_lastname").send_keys('Andrade')
+        driver.find_element_by_id("passwd").send_keys('123456')
 
-    time.sleep(2)
+        time.sleep(2)
 
-    driver.find_element_by_id("company").send_keys('Mercado Eletronico')
-    driver.find_element_by_id("city").send_keys('Sao Paulo')
-    driver.find_element_by_id("address1").send_keys('Rua 1')
-    driver.find_element_by_id("address2").send_keys('Rua 2')
-    driver.find_element_by_id("postcode").send_keys('00000')
-    driver.find_element_by_id("id_state").send_keys('Alabama')
-    driver.find_element_by_id("phone").send_keys('(11) 1111-1111')
-    driver.find_element_by_id("phone_mobile").send_keys('(22) 2 2222-2222')
+        driver.find_element_by_id("days").send_keys('1')
+        driver.find_element_by_id("months").send_keys('setembro')
+        driver.find_element_by_id("years").send_keys('1994')
 
+        time.sleep(2)
+
+        driver.find_element_by_id("optin").click()
+        driver.find_element_by_id("newsletter").click()
+
+        time.sleep(2)
+
+        driver.find_element_by_id("company").send_keys('Mercado Eletronico')
+        driver.find_element_by_id("city").send_keys('Sao Paulo')
+        driver.find_element_by_id("address1").send_keys('Rua 1')
+        driver.find_element_by_id("address2").send_keys('Rua 2')
+        driver.find_element_by_id("postcode").send_keys('00000')
+        driver.find_element_by_id("id_state").send_keys('Alabama')
+        driver.find_element_by_id("phone").send_keys('(11) 1111-1111')
+        driver.find_element_by_id("phone_mobile").send_keys('(22) 2 2222-2222')
+        driver.find_element_by_id(
+            "submitAccount").send_keys('(22) 2 2222-2222')
+
+        comandoSql = "INSERT INTO usuario_login(email, nome, senha) VALUES(%s, %s, %s)"
+        valores = (recebeEmail, 'Joao', '123456')
+        cursor.execute(comandoSql, valores)
+        conDb.commit()
+
+    else:
+        print("email cadastrado")
 
 elif resp == 2:
 
@@ -98,5 +109,11 @@ elif resp == 2:
         "http://automationpractice.com/index.php?controller=authentication&back=my-account")
 
     driver.find_element_by_id("email").send_keys(recebeEmail)
-    driver.find_element_by_id("passwd").send_keys('teste')
+    driver.find_element_by_id("passwd").send_keys('123456')
     driver.find_element_by_id(SubmitLogin).click()
+
+
+if conDb.is_connected():
+    cursor.close()
+    conDb.close()
+    print("Conexão encerrada")
